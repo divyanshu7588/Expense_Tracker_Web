4dc48ba851ff86036277c819d2d51373c31b08d0
divyanshu7588 <138211414+divyanshu7588@users.noreply.github.com>
2025-09-12 13:30:55 +0530

    Suceesfully added login and Register Api

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
+from .utils import hash_password, check_password, generate_jwt
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if not username or not email or not password:
+            return Response({"error": "All fields are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "Username already taken"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(email=email).exists():
+            return Response({"error": "Email already registered"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = User.object.create(
+            username=username,
+            email=email,
+            password=hash_password(password)
+            
+        )
+        
+        serializer =UserSerializer(user)
+        return Response(serializer.data , status=status.HTTP_201_CREATED)
+    
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response({"error": "Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({"error": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        if not check_password(password, user.password):
+            return Response({"error": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        token = generate_jwt(user.id)
+        serializer = UserSerializer(user)
+        return Response({"token": token, "user": serializer.data}, status=status.HTTP_200_OK)
